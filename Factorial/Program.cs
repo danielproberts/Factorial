739504dd08b62cc3b97299251047e46b4eb6dcd8
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-11-07 13:12:33 -0500

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+
+            for (int i = num; i > 0; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("{0}! = {1}", num, factorial);
+
+            Console.WriteLine("Press any key to close window...");
+            Console.ReadKey();
+        }
+    }
+}
